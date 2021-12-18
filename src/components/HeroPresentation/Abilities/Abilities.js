dab5afa808b2f5b59c95191b4f8ffc315b570d94
alan <alan.fer09@gmail.com>
2021-12-18 11:42:14 -0300

    add logic

--- src/components/HeroPresentation/Abilities/Abilities.js
+++ src/components/HeroPresentation/Abilities/Abilities.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './abilities.module.css'
-import ReactHtmlParser from 'react-html-parser';
 
+import InformationSkill from "../InformationSkill/InformationSkill";
 const Abilities = ({habilities})=>{
     const mouseEnter = (id, index)=>{
         const infoSkill = document.getElementById(id+'_info')
@@ -33,69 +33,7 @@ const Abilities = ({habilities})=>{
                                    onMouseEnter={()=>mouseEnter(ability.name,index)}
                                    onMouseLeave={()=>mouseLeave(ability.name)}
                                  >
-                                    <section 
-                                    className={classes.info_skill} 
-                                    id={ability.name+'_info'}
-                                    >
-                                    <div className={classes.tittle_ability}>
-                                        <span>
-                                            {ability?.name_loc.toUpperCase()}
-                                        </span>
-                                    </div>
-                                    <div className={classes.sub_skill_info}>{
-                                        ability?.damage ? (
-                                            <>
-                                        <span className={classes.skill_label}>
-                                            TIPO DE DAÑO : 
-                                        </span>
-                                        <span style={{
-                                            color:ability?.damage === 1 ? 'red' : 'rgb(163, 220, 238)',
-                                            fontSize:12,
-                                            letterSpacing:2,
-                                            marginLeft:5
-                                        }}>
-                                            {ability?.damage === 1 ? 'Físico' : 'Mágico'}
-                                        </span>
-                                            </>
-                                        ) : null
-                                        }
-                                    </div>
-                                    <div className={classes.text_info_skill}>
-                                        {ReactHtmlParser(ability.desc_loc)}
-                                    </div>
-                                    <div style={{display:'flex',flexDirection:'column'}}>
-                                        {ability.special_values.map(value=>{
-                                            return(
-                                                value.heading_loc ? (
-                                                    <section 
-                                                     className={classes.center_element}
-                                                     key={value.name}
-                                                     style={{marginBottom:5}}
-                                                     >
-                                                        <div className={classes.skill_label}>
-                                                            {value.heading_loc}
-                                                        </div>
-                                                        <span className={classes.special_value}>
-                                                            {value.values_int.length > 0 
-                                                            ? value.values_int.join(' /')
-                                                            : (value.values_int[0] 
-                                                                ? value.values_int[0].toFixed(1) 
-                                                                : value.values_float[0].toFixed(1))}
-                                                        </span>
-                                                    </section>
-                                                ) : null
-                                            )
-                                        })}
-                                    </div>
-                                    <div style={{
-                                        display: 'flex',
-                                        alignItems: 'center'}}>
-                                        <div className={classes.mana_icon}></div>
-                                        <span className={classes.special_value}>{ability.mana_costs.length > 0 
-                                        ? ability.mana_costs.join(' /')
-                                        : ability.mana_costs[0] }</span>
-                                    </div>
-                                    </section>
+                                    <InformationSkill ability={ability}/>
                                  </div>
                                 <img
                                 id={ability.name+'_img'}
--- src/components/HeroPresentation/HeroPresentation.js
+++ src/components/HeroPresentation/HeroPresentation.js
@@ -3,6 +3,8 @@ import classes from './heroPresentation.module.css'
 import { HeroesContex } from "../../contexts/HeroesContext";
 import { HeroContex } from "../../contexts/HeroContext";
 import Abilities from "./Abilities/Abilities";
+import ReactHtmlParser from 'react-html-parser';
+
 const HeroPresentation =()=>{
     const [heroData,setHeroData] = useState(null)
     const heroContext = useContext(HeroContex)
@@ -23,51 +25,93 @@ const HeroPresentation =()=>{
     return (
         <>
         <div className={classes.presentation_container}>
-            <section className={classes.hero_name}>
-                <div>
+            <section className={classes.container_hero_name}>
+                <div className={classes.img_container}>
                     <img
                      src={`${srcMultimedia}/images/dota_react/heroes/${heroData?.name?.replace('npc_dota_hero_','')}.png`}
                      className={classes.profile_img}
                      />
                 </div>
-                <div className={classes.tittle_hero_name}>
                 <div className={classes.container_tittle_name}>
-                    <span>
-                        <img
-                        style={{width:35,margin:5}}
-                         src={`${srcMultimedia}/images/dota_react/icons/hero_${attr[heroData?.primary_attr]}.png`}
-                         />
-                    </span>
-                    <span>
+                    <span className={classes.hero_name}>
                         {heroData?.name_loc?.toUpperCase()}
                     </span>
+                  
                 </div>
+                <span className={classes.img_attr}>
+                        <img
+                        style={{width:35,margin:5,backgroundColor: '#000000a1',
+                            borderRadius: '50%'}}
+                         src={`${srcMultimedia}/images/dota_react/icons/hero_${attr[heroData?.primary_attr]}.png`}
+                         />
+                </span>
+                <section className={classes.attack}>
+                    <section>
+                        <div className={classes.statsInfo}>
+                            <img className={classes.icon_stats} src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_damage.png`}/>
+                            <span style={{marginLeft:10}}>
+                                {heroData?.damage_min}{'-'}{heroData?.damage_max}
+                            </span>
+                        </div>
+                        <div className={classes.statsInfo}>
+                            <img className={classes.icon_stats} src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_armor.png`}/>
+                            <span style={{marginLeft:10}}>
+                                {heroData?.armor?.toFixed(1)}
+                            </span>
+                        </div>
+                        <div className={classes.statsInfo}>
+                            <img className={classes.icon_stats} src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_movement_speed.png`}/>
+                            <span style={{marginLeft:10}}>
+                                {heroData?.movement_speed}
+                            </span>
+                        </div>
+                    </section>
+                    <div style={{display:'flex',flexDirection:'column'}}>
+                        <section style={{display:'flex',marginTop:5,justifyContent:'space-evenly'}}>
+                            <img 
+                            src={`${srcMultimedia}/images/dota_react/icons/${attackCapabilityIcon[heroData?.attack_capability]}.svg`}
+                            style={{width:20}}
+                            />
+                            <span style={{marginLeft:10}}>{attackCapability[heroData?.attack_capability]}</span>
+                        </section>
+                    </div>
+                </section>
+                <section className={classes.attr}>
+                    <div className={classes.attrContainer} >
+                        <img style={{width:25,height:25}}src={`${srcMultimedia}/images/dota_react/icons/hero_strength.png`}/>
+                        <span className={classes.attrInfo}>
+                            {heroData ? heroData.str_base : 0}
+                        </span>
+                        <span className={classes.attrGain}>
+                            + {heroData ? heroData?.str_gain?.toFixed(1) : 0}
+                        </span>
+                    </div>
+                    <div className={classes.attrContainer}>
+                                <img style={{width:25,height:25}}src={`${srcMultimedia}/images/dota_react/icons/hero_agility.png`}/>
+                                <span className={classes.attrInfo}>
+                                {heroData ? heroData.agi_base : 0}
+                                </span>
+                                <span className={classes.attrGain}>
+                                 + {heroData ? heroData?.agi_gain?.toFixed(1) : 0}
+                                 </span>
+                            </div>
+                            <div className={classes.attrContainer}>
+                                <img style={{width:25,height:25}}src={`${srcMultimedia}/images/dota_react/icons/hero_intelligence.png`}/>
+                                <span className={classes.attrInfo}>
+                                {heroData ? heroData.int_base : 0}
+                                </span>
+                                <span className={classes.attrGain}>
+                                 + {heroData ? heroData?.int_gain?.toFixed(1) : 0}
+                                 </span>
+                            </div>
+                        </section>
+                    
+                {/*
+
                 <span style={{fontSize:15,color:'#ffffff8c',letterSpacing:2}}>
                      Heroe de {attrEsp[heroData?.primary_attr]}
                 </span>
-                </div>
-                <div>
-                <section>
-                    <div className={classes.statsInfo}>
-                        <img src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_damage.png`}/>
-                        <span style={{marginLeft:10}}>
-                            {heroData?.damage_min}{'-'}{heroData?.damage_max}
-                        </span>
-                                </div>
-                                <div className={classes.statsInfo}>
-                                    <img src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_armor.png`}/>
-                                    <span style={{marginLeft:10}}>
-                                        {heroData?.armor?.toFixed(1)}
-                                    </span>
-                                </div>
-                                <div className={classes.statsInfo}>
-                                    <img src={`${srcMultimedia}/images/dota_react//heroes/stats/icon_movement_speed.png`}/>
-                                    <span style={{marginLeft:10}}>
-                                        {heroData?.movement_speed}
-                                    </span>
-                                </div>
-                        </section>
-                </div>
+                */}
             </section>
             <video
             src={`${srcMultimedia}/videos/dota_react/heroes/renders/${heroData? heroData.name.replace('npc_dota_hero_',''):'antimage'}.webm`} type="video/mp4"
@@ -88,50 +132,13 @@ const HeroPresentation =()=>{
                 display:'flex',
                 flexDirection:'column'
                 }}>
-                    <section className={classes.infoHero}>
-                        <section className={classes.attack}>
-                            <div style={{display:'flex',flexDirection:'column'}}>
-                                <section>
-                                    <span style={{color:'#ffffff8c'}}>TIPO DE ATAQUE:</span>
-                                </section>
-                                <section style={{display:'flex',marginTop:5,justifyContent:'space-between'}}>
-                                <img src={`${srcMultimedia}/images/dota_react/icons/${attackCapabilityIcon[heroData?.attack_capability]}.svg`}/>
-                                <span>{attackCapability[heroData?.attack_capability]}</span>
-                                </section>
-                            </div>
+                    <div className={classes.container_info_hero}>
+                        <section className={classes.hype_loc}>
+                            {ReactHtmlParser(heroData?.hype_loc)}
                         </section>
-                        <section className={classes.attr}>
-                            <div className={classes.attrContainer} >
-                                <img style={{width:35,height:35}}src={`${srcMultimedia}/images/dota_react/icons/hero_strength.png`}/>
-                                <span className={classes.attrInfo}>
-                                    {heroData ? heroData.str_base : 0}
-                                </span>
-                                <span className={classes.attrGain}>
-                                     + {heroData ? heroData?.str_gain?.toFixed(1) : 0}
-                                </span>
-                            </div>
-                            <div className={classes.attrContainer}>
-                                <img style={{width:35,height:35}}src={`${srcMultimedia}/images/dota_react/icons/hero_agility.png`}/>
-                                <span className={classes.attrInfo}>
-                                {heroData ? heroData.agi_base : 0}
-                                </span>
-                                <span className={classes.attrGain}>
-                                 + {heroData ? heroData?.agi_gain?.toFixed(1) : 0}
-                                 </span>
-                            </div>
-                            <div className={classes.attrContainer}>
-                                <img style={{width:35,height:35}}src={`${srcMultimedia}/images/dota_react/icons/hero_intelligence.png`}/>
-                                <span className={classes.attrInfo}>
-                                {heroData ? heroData.int_base : 0}
-                                </span>
-                                <span className={classes.attrGain}>
-                                 + {heroData ? heroData?.int_gain?.toFixed(1) : 0}
-                                 </span>
-                            </div>
-                        </section>
-                    </section>
-                    <div className={classes.tittle}>
-                        <span>HABILIDADES</span>
+                        <div className={classes.tittle}>
+                            <span>HABILIDADES</span>
+                        </div>
                     </div>
                    <Abilities habilities={heroData?.abilities}/>
             </div>
